f04fd1baeca7a58ec73f825bc655e6efc7005d3e
Philipp Hancke <fippo@appear.in>
2017-07-01 16:45:08 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# karma-stability
+karma-stability is a reporter for karma. It allows comparing test results
+against a known set of expectations generated from a previous run.
+
+Inspired by https://github.com/w3c/wptrunner
---
+++ stability.js
@@ -0,0 +1,51 @@
+/* based on this excellent tutorial:
+ * https://www.ironsrc.com/news/how-to-create-a-custom-karma-reporter-3/
+ */
+const fs = require('fs');
+function StabilityReporter(baseReporterDecorator, config, logger) {
+    baseReporterDecorator(this);
+    let log = logger.create('reporter.stability');
+    let tests = [];
+    this.specSuccess = function(browser, result) {
+        tests.push(['PASS', result.suite.join(' '),
+            result.description].join(' '));
+    };
+    this.specSkipped = function(browser, result) {
+        tests.push(['SKIP', result.suite.join(' '),
+            result.description].join(' '));
+    };
+    this.specFailure = function(browser, result) {
+        tests.push(['ERR', result.suite.join(' '),
+            result.description,
+            result.assertionErrors[0].name].join(' '));
+    };
+    this.onRunComplete = function(browserCollection, results) {
+        if (results.error || results.disconnected) {
+            return;
+        }
+        const expected = fs.readFileSync(config.stabilityReporter.path,
+            {encoding: 'ascii'});
+        // Set the exit code so that only unexpected failures result in
+        // a non-zero exit code.
+        results.exitCode = expected.trim() === tests.join('\n') ? 0 : 1;
+        log.info('DONE', process.env.BROWSER, process.env.BVER,
+            'success=' + results.success,
+            'failed=' + results.failed,
+            'expected=' + (results.exitCode === 0));
+
+        // When a test fails show the results of the test.
+        // TODO: make a nicer diff.
+        if (results.exitCode !== 0) {
+            log.error('Mismatch in expectations.');
+            tests.forEach(function(t) {
+                console.log(t);
+            });
+        }
+    };
+};
+
+StabilityReporter.$inject = ['baseReporterDecorator', 'config', 'logger'];
+
+module.exports = {
+    'reporter:stability': ['type', StabilityReporter]
+};
